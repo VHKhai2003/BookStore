43dd60b335280eb4be341f998abbb81ad12a64c2
21120477 <vuonghuynhkhai4869@gmail.com>
2024-08-13 14:48:57 +0700

    init project & add main ui layout

---
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.example.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class BookController {
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index";
+    }
+}
